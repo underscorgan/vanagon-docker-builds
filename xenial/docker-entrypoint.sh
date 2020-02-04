7a4b606972f8bb9be45a57bd8c37b7afcdb4866b
Morgan Rhodes <morgan@puppet.com>
2020-02-04 14:31:50 -0800

    Add artifacts for xenial

---
+++ xenial/docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+message="\$REPO_URL and \$VANAGON_PROJECT are required environment variables"
+
+if [ -z "$REPO_URL" ]; then
+  echo $message
+  exit 1
+elif [ -z "$VANAGON_PROJECT" ]; then
+  echo $message
+  exit 1
+fi
+
+directory="${REPO_URL##*/}"
+
+# clone the git repo
+git clone "$REPO_URL"
+cd "$directory"
+git checkout "$REPO_REF"
+
+bundle install
+bundle exec /usr/local/bin/build --engine local "$@" "$VANAGON_PROJECT" ubuntu-16.04-amd64
+
+find output -type f -exec cp {} /artifacts \;
